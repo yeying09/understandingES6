62cd5b1c50bb7650baee4f4aa20c65e5afc79000
yeying <ying.ye@ecreditpal.com>
2018-03-13 18:55:36 +0800

    first commit

---
+++ ch1/block-binding.js
@@ -0,0 +1,17 @@
+function getValue(condition) {
+	if(condition) {
+		let value = "blue";
+
+		console.log(value);	
+
+		return value;
+	} else {
+		console.log(value);
+
+		return null;
+	}
+
+	console.log(value);
+}
+
+getValue(false);
